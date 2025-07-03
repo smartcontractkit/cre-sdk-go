76de2d896ebf2e12caf79915d72ed202312b6e87
Ryan Tinianov <tinianov@live.com>
2025-07-03 13:08:41 -0400

    Generate the capabilites (#9)

---
+++ capabilities/scheduler/cron/generate/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/smartcontractkit/chainlink-common/pkg/values/installer/pkg"
+    "github.com/smartcontractkit/cre-sdk-go/generator/protos"
+)
+
+func main() {
+    gen, err := protos.NewGeneratorAndInstallToolsForCapability()
+    if err != nil {
+        panic(err)
+    }
+	if err := gen.Generate(&pkg.CapabilityConfig{
+		Category:     "scheduler",
+		Pkg:          "cron",
+		MajorVersion: 1,
+		Files: []string{
+			"trigger.proto",
+		},
+	}); err != nil {
+		panic(err)
+	}
+}
---
+++ capabilities/scheduler/cron/go.mod
@@ -0,0 +1,15 @@
+module github.com/smartcontractkit/cre-sdk-go/capabilities/scheduler/cron
+
+go 1.24.4
+
+require (
+	github.com/smartcontractkit/chainlink-common/pkg/values v0.0.0-20250703141104-e7b4552635a9
+	github.com/smartcontractkit/cre-sdk-go v0.0.0-20250703152056-92306f71577b
+	google.golang.org/protobuf v1.36.6
+)
+
+require (
+	github.com/go-viper/mapstructure/v2 v2.3.0 // indirect
+	github.com/shopspring/decimal v1.4.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+)
