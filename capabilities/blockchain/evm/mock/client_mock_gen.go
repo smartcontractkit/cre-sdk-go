10b0d39bc0bac81678a0bdd15b1efb67f7ad886a
Michael Street <5597260+MStreet3@users.noreply.github.com>
2025-07-16 13:46:00 -0400

    run generate

--- capabilities/blockchain/evm/mock/client_mock_gen.go
+++ capabilities/blockchain/evm/mock/client_mock_gen.go
@@ -9,9 +9,9 @@ import (
 
 	"google.golang.org/protobuf/types/known/anypb"
 
-	"google.golang.org/protobuf/types/known/emptypb"
-
 	"github.com/smartcontractkit/cre-sdk-go/capabilities/blockchain/evm"
+
+	"google.golang.org/protobuf/types/known/emptypb"
 
 	sdkpb "github.com/smartcontractkit/chainlink-common/pkg/workflows/sdk/v2/pb"
 	"github.com/smartcontractkit/cre-sdk-go/sdk/testutils/registry"
