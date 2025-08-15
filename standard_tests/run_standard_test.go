d051f4a3d59447cbc29edda06720af0614c539dd
Dylan Tinianov <dylan.tinianov@smartcontract.com>
2025-08-15 14:51:42 -0400

    Use common commit hash

--- standard_tests/run_standard_test.go
+++ standard_tests/run_standard_test.go
@@ -12,7 +12,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-const commonVersion = "cre-std-tests@0.3.0"
+const commonVersion = "b4b2af4fa578ab2ae6eda980605073dece26ad53"
 
 func TestRunStandardTest(t *testing.T) {
 	t.Parallel()
