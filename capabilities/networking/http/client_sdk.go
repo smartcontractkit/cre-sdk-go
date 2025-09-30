47668a5605bf3fde36cefc26eb84b1773624d974
Ryan Tinianov <tinianov@live.com>
2025-09-30 09:25:33 -0400

    Make http report sending more flexible (#71)

--- capabilities/networking/http/client_sdk.go
+++ capabilities/networking/http/client_sdk.go
@@ -1,60 +1,23 @@
 package http
 
 import (
-	"encoding/binary"
-
+	"github.com/smartcontractkit/chainlink-protos/cre/go/sdk"
 	"github.com/smartcontractkit/cre-sdk-go/cre"
-	"google.golang.org/protobuf/types/known/durationpb"
 )
 
-type ReportRequest struct {
-	Url     string
-	Method  string
-	Headers map[string]string
-	Report  *cre.Report
-	Timeout *durationpb.Duration
-
-	// CacheSettings is more limited than on a Request, as reports may contain different sets of signatures on different nodes, leading to a cache miss.
-	CacheSettings *CacheSettings
-}
-
-// SendReport functions the same as SendRequest, but takes a [cre.Report] as the body.
+// SendReport functions the same as SendRequest, but takes a [cre.Report] and a function to convert the inner [sdk.ReportResponse] to a [Request].
 // Note that caching is limited as reports may contain different sets of signatures on different nodes, leading to a cache miss.
-func (c *SendRequester) SendReport(input *ReportRequest) cre.Promise[*Response] {
-	return c.SendRequest(reportRequestToRequest(input))
+func (c *SendRequester) SendReport(report cre.Report, fn func(*sdk.ReportResponse) (*Request, error)) cre.Promise[*Response] {
+	return c.client.SendReport(c.nodeRuntime, report, fn)
 }
 
-// SendReport functions the same as SendRequest, but takes a [cre.Report] as the body.
+// SendReport functions the same as SendRequest, but takes a [cre.Report] and a function to convert the inner [sdk.ReportResponse] to a [Request].
 // Note that caching is limited as reports may contain different sets of signatures on different nodes, leading to a cache miss.
-func (c *Client) SendReport(runtime cre.NodeRuntime, input *ReportRequest) cre.Promise[*Response] {
-	return c.SendRequest(runtime, reportRequestToRequest(input))
-}
-
-// Id of the node is a uint32 stored in 4 bytes.
-const nodeIdLen = 4
-
-func reportRequestToRequest(in *ReportRequest) *Request {
-	report := in.Report.X_GeneratedCodeOnly_Unwrap()
-	sigLen := 0
-	if len(report.Sigs) != 0 {
-		sigLen = len(report.Sigs) * (len(report.Sigs[0].Signature) + nodeIdLen)
+func (c *Client) SendReport(runtime cre.NodeRuntime, report cre.Report, fn func(*sdk.ReportResponse) (*Request, error)) cre.Promise[*Response] {
+	rawReport := report.X_GeneratedCodeOnly_Unwrap()
+	input, err := fn(rawReport)
+	if err != nil {
+		return cre.PromiseFromResult[*Response](nil, err)
 	}
-	body := make([]byte, len(report.RawReport)+len(report.ReportContext)+sigLen)
-	copy(body, report.RawReport)
-	copy(body[len(report.RawReport):], report.ReportContext)
-	pos := len(report.RawReport) + len(report.ReportContext)
-	for _, sig := range report.Sigs {
-		copy(body[pos:], sig.Signature)
-		pos += len(sig.Signature)
-		binary.LittleEndian.PutUint32(body[pos:], sig.SignerId)
-		pos += 4
-	}
-	return &Request{
-		Url:           in.Url,
-		Method:        in.Method,
-		Headers:       in.Headers,
-		Body:          body,
-		Timeout:       in.Timeout,
-		CacheSettings: in.CacheSettings,
-	}
+	return c.SendRequest(runtime, input)
 }
