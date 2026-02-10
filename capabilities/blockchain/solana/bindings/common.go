eb63f517a7279fefa53edaf3b912d6a2d9b44653
yashnevatia <yashvardhan.nevatia@smartcontract.com>
2026-02-10 11:49:41 +0000

    add fwdReport marshal

--- capabilities/blockchain/solana/bindings/common.go
+++ capabilities/blockchain/solana/bindings/common.go
@@ -1,7 +1,12 @@
 package bindings
 
 import (
+	"bytes"
 	"crypto/sha256"
+	"fmt"
+
+	"github.com/gagliardetto/anchor-go/errors"
+	binary "github.com/gagliardetto/binary"
 
 	"github.com/smartcontractkit/cre-sdk-go/capabilities/blockchain/solana"
 )
@@ -9,6 +14,30 @@ import (
 type ForwarderReport struct {
 	AccountHash [32]byte
 	Payload     []byte
+}
+
+func (obj ForwarderReport) MarshalWithEncoder(encoder *binary.Encoder) (err error) {
+	// Serialize `AccountHash`:
+	err = encoder.Encode(obj.AccountHash)
+	if err != nil {
+		return errors.NewField("AccountHash", err)
+	}
+	// Serialize `Payload`:
+	err = encoder.Encode(obj.Payload)
+	if err != nil {
+		return errors.NewField("Payload", err)
+	}
+	return nil
+}
+
+func (obj ForwarderReport) Marshal() ([]byte, error) {
+	buf := bytes.NewBuffer(nil)
+	encoder := binary.NewBorshEncoder(buf)
+	err := obj.MarshalWithEncoder(encoder)
+	if err != nil {
+		return nil, fmt.Errorf("error while encoding ForwarderReport: %w", err)
+	}
+	return buf.Bytes(), nil
 }
 
 func CalculateAccountHash(accs []*solana.AccountMeta) [32]byte {
--- capabilities/blockchain/solana/go.mod
+++ capabilities/blockchain/solana/go.mod
@@ -9,6 +9,18 @@ require (
 )
 
 require (
+	github.com/blendle/zapdriver v1.3.1 // indirect
+	github.com/gagliardetto/anchor-go v1.0.0 // indirect
+	github.com/gagliardetto/binary v0.8.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
+	github.com/logrusorgru/aurora v2.0.3+incompatible // indirect
+	github.com/mitchellh/go-testing-interface v1.14.1 // indirect
 	github.com/shopspring/decimal v1.4.0 // indirect
+	github.com/streamingfast/logging v0.0.0-20230608130331-f22c91403091 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.21.0 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 )
