e2215d14502ce62ed5b1b39f9d7e9a4dd21cb36c
amit-momin <108959691+amit-momin@users.noreply.github.com>
2026-05-21 12:05:12 -0500

    Added private-testnet-rhyolite support (#139)

--- capabilities/blockchain/evm/client_sdk_gen.go
+++ capabilities/blockchain/evm/client_sdk_gen.go
@@ -416,6 +416,8 @@ const PolygonTestnetAmoy = 16281711391670634445
 
 const PrivateTestnetAndesite = 6915682381028791124
 
+const PrivateTestnetRhyolite = 604447335222770945
+
 const SonicMainnet = 1673871237479749969
 
 const SonicTestnet = 1763698235108410440
@@ -524,6 +526,8 @@ func ChainSelectorFromName(name string) (uint64, error) {
 		return PolygonTestnetAmoy, nil
 	case "private-testnet-andesite":
 		return PrivateTestnetAndesite, nil
+	case "private-testnet-rhyolite":
+		return PrivateTestnetRhyolite, nil
 	case "sonic-mainnet":
 		return SonicMainnet, nil
 	case "sonic-testnet":
