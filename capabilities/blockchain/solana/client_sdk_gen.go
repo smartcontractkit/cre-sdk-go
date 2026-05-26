043b75d05749e22c06beda8bf27fc54065e6ad96
Silas Lenihan <32529249+silaslenihan@users.noreply.github.com>
2026-05-26 17:10:07 -0400

    Solana LogTrigger: Support Anchor >0.3 and CPIFilters (#114)
    * Solana LogTrigger: Change eventIdlJson to contractIdlJson
    
    * Add cpi filter config (#115)
    
    * Solana Client: Add CPI Filter Config
    
    * bump go.mod
    
    * bump go.mod
    
    * bump go.mod and regen

--- capabilities/blockchain/solana/client_sdk_gen.go
+++ capabilities/blockchain/solana/client_sdk_gen.go
@@ -354,18 +354,10 @@ func (c *Client) WriteReport(runtime cre.Runtime, input *WriteCreReportRequest) 
 
 const SolanaDevnet = 16423721717087811551
 
-const SolanaMainnet = 124615329519749607
-
-const SolanaTestnet = 6302590918974934319
-
 func ChainSelectorFromName(name string) (uint64, error) {
 	switch name {
 	case "solana-devnet":
 		return SolanaDevnet, nil
-	case "solana-mainnet":
-		return SolanaMainnet, nil
-	case "solana-testnet":
-		return SolanaTestnet, nil
 	default:
 		return 0, errors.New("unknown ChainSelector" + name)
 	}
