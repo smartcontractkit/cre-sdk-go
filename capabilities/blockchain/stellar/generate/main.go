d8422a8cc87d95451f8afa50ac3baa2a259fba85
ilija42 <pavlovicilija42@gmail.com>
2026-07-01 19:27:23 +0200

    Add stellar client

---
+++ capabilities/blockchain/stellar/generate/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/smartcontractkit/chainlink-protos/cre/go/installer/pkg"
+	"github.com/smartcontractkit/cre-sdk-go/generator/protos"
+)
+
+func main() {
+	gen, err := protos.NewGeneratorAndInstallToolsForCapability()
+	if err != nil {
+		panic(err)
+	}
+	if err := gen.Generate(&pkg.CapabilityConfig{
+		Category:      "blockchain",
+		Pkg:           "stellar",
+		MajorVersion:  1,
+		PreReleaseTag: "alpha",
+		Files: []string{
+			"scval.proto",
+			"client.proto",
+		},
+	}); err != nil {
+		panic(err)
+	}
+}
